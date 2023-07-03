fa532243dc9a04ee34478ef97d49b0e4e95fe630
Andrew-Infinite <81764717+Andrew-Infinite@users.noreply.github.com>
2023-07-03 21:39:20 +0900

    Update HW 1-2 Bigram Language Model.py

--- HW1/HW 1-2 Bigram Language Model.py
+++ HW1/HW 1-2 Bigram Language Model.py
@@ -5,47 +5,49 @@ text = "<s> i am at hiroshima university </s> <s> i live in hiroshima </s> <s> i
 text2 = "<s> i live in an apartment </s>"
 
 # Tokenize the text into sentences
-words =  text.split()
-words2 = text2.split()
+train =  text.split()
+test = text2.split()
 
-# Tokenize each sentence into words
-tokenized_word = [word for word in words]
-tokenized_word2 = [word2 for word2 in words2]
-tokenized_word2 = tokenized_word + tokenized_word2
+BagOfWords = set(word for word in (train + test))
 
-# Initialize a dictionary to store the bigram counts
 bigram_counts = defaultdict(int)
 word_counts = defaultdict(int)
-word_counts2 = defaultdict(int)
 preceding_word = None
-preceding_word2 = None
 
 # counting words, and counting pair words
-for word in tokenized_word:
+for word in train:
     if preceding_word is not None:
         current_word = word
         bigram_counts[(preceding_word, current_word)] += 1
-        word_counts[current_word] += 1
+    word_counts[word] += 1
     preceding_word = word
 
-for word2 in tokenized_word2:
-    if preceding_word2 is not None:
-        current_word2 = word2
-        if bigram_counts.get((preceding_word2, current_word2))==None:
-          bigram_counts[(preceding_word2, current_word2)] = 0
-        word_counts2[current_word2] += 1
-    preceding_word2 = word2
-
-bigram_probabilities = {}
-bigram_with_Laplace = {}
+bigram_probabilities_train = {}
+bigram_with_Laplace_train = {}
 
 # Iterate over the bigram counts
 for bigram, count in bigram_counts.items():
     try:
-      bigram_probabilities[bigram] = count / word_counts[bigram[1]]
+        bigram_probabilities_train[bigram] = count / word_counts[bigram[0]]
     except:
-      print("skip")
-    bigram_with_Laplace[bigram] = (count+1)/(word_counts[bigram[1]] +len(word_counts2))
+        #Zero Frequency
+        print("skip")
+    bigram_with_Laplace_train[bigram] = (count+1)/(word_counts[bigram[0]] + len(BagOfWords))
+print(bigram_probabilities_train)
+print(bigram_with_Laplace_train)
 
-print(bigram_probabilities)
-print(bigram_with_Laplace)
+bigram_probabilities_test = {}
+bigram_with_Laplace_test = {}
+preceding_word = None
+for word in test:
+    if preceding_word is not None:
+        current_word = word
+        try:
+            bigram_probabilities_test[(preceding_word, current_word)] = bigram_probabilities_train[(preceding_word, current_word)]
+            bigram_with_Laplace_test[(preceding_word, current_word)] = bigram_with_Laplace_train[(preceding_word, current_word)]
+        except:
+            bigram_probabilities_test[(preceding_word, current_word)] = float('inf')
+            bigram_with_Laplace_test[(preceding_word, current_word)] = 1.0/(word_counts[preceding_word] + len(BagOfWords))
+    preceding_word = word
+print(bigram_probabilities_test)
+print(bigram_with_Laplace_test)
